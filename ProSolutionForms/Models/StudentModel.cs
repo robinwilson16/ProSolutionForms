85c51eb62f4070272a8a44de5cc27074aa251b3a
Robin Wilson <robin.wilson@shcg.ac.uk>
2024-11-25 17:43:33 +0000

    Initial Version

---
+++ ProSolutionForms/Models/StudentModel.cs
@@ -0,0 +1,13 @@
+﻿namespace ProSolutionForms.Models
+{
+    public class StudentModel
+    {
+        public int StudentID { get; set; }
+        public int? StudentDetailID { get; set; }
+        public string? StudentRef { get; set; }
+        public string? Surname { get; set; }
+        public string? Forename { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public Guid? StudentGUID { get; set; }
+    }
+}
